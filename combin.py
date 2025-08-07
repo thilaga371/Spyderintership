1602b3a0420116fbd44a435eca4423ff7c1435ee
thilaga371 <thilagatj43>
2025-08-07 12:53:18 +0530

    success combin

---
+++ combin.py
@@ -0,0 +1,17 @@
+def fun(**No):
+    for i,j in No.items():
+        print(i,j)
+
+fun( numbers=[1,2,3,4,5],letters=('a','b','c','d'))
+
+
+
+        
+number=[1,2,3,4,5]
+for a in number:
+    print(a)
+
+letter=('a','b','c','d')
+for b in letter:
+    print(b)
+

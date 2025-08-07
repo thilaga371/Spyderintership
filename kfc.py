521de14a3c52c67c8db20e749d3b97898e4177ec
thilaga371 <thilagatj43@gmail.com>
2025-08-07 13:08:46 +0530

    kfc

---
+++ kfc.py
@@ -0,0 +1,7 @@
+day=input("enter a day:")
+amount=599
+food="c.burger"
+if(day in["wed","sun"])and(amount>=599)and(food=="c.burger"):
+    print("your eligible for 70%_discount")
+else:
+    print("your not eligible for 70%_discount")
